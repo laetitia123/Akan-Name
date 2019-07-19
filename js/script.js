ce58ba4522f7f138aea4488bc9a9d7c270c7df45
laetitia123 <uwizelaetitia@gmail.com>
2019-07-19 16:14:31 +0200

    my first commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+function calculate(form){
+        var days=["Kwasi", "Kwadwo", "Kwabena", "Kwaku","Yaw",  "Kofi Kwame"];
+        var names=["Akosua","Adwoa", "Abenaa", "Akua", "Yaa","Afua Ama"]
+        
+        var year = document.forms["form1"]["year"].value;
+        var gender = document.forms["form1"]["gender"].value;
+        var month= document.forms["form1"]["month"].value;
+        var day= document.forms["form1"]["day"].value;
+        var yy=parseInt(year);
+        var dd=parseInt(day);
+        var mm=parseInt(month);
+        var CC=(year-1)/100+1;
+        var d=( ( (CC/4)-2*CC-1)+((5*yy/4) )+((26*(mm+1)/10)) +dd )%7;
+        
+        if (year == ""|| gender=="")
+        
+        {
+        alert("Please fill all ");
+        
+        }
+        else if (month == ""|| day=="")
+        {
+        alert("Please fill all ");
+        
+        }
+        else if(month<=0||month >12){
+        alert("please fill the correct month input");
+        }
+        else if(day <=0|| day>31){
+          alert("please fill the correct day input");
+        }
+        else if (year <=0){
+          alert("please fill the correct year");
+        }
+           else{
+          //  return (Math.round(d));
+           if (d=5 && gender=="female"){
+             alert("you are born on friday and your name is yaw");
+            //  document.getElementById("demo").innerHTML=("you are born on friday");
+           }
+            else if(d=5 && gender=="male"){
+              alert("waaw");
+            }
+           else{
+             alert("jdfhdfkdof");
+           }
+            }
+          
+          }
